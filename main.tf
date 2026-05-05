86d802436ecfa968d1cae756801954c50805c69a
AnumoyMurmu <majhianumoy8@gmail.com>
2026-05-05 18:29:58 +0530

    feat: add monitoring stack (grafana/prometheus/cadvisor)

--- main.tf
+++ main.tf
@@ -73,3 +73,68 @@ resource "docker_container" "aethervault" {
 
   restart = "unless-stopped"
 }
+
+
+# 1. cAdvisor (The Container Watcher)
+resource "docker_container" "cadvisor" {
+  name  = "cadvisor"
+  image = "gcr.io/cadvisor/cadvisor:latest"
+  restart = "unless-stopped"
+  
+  volumes {
+    host_path      = "/"
+    container_path = "/rootfs"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/var/run"
+    container_path = "/var/run"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/sys"
+    container_path = "/sys"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/var/lib/docker"
+    container_path = "/var/lib/docker"
+    read_only      = true
+  }
+
+  networks_advanced {
+    name = docker_network.aether_network.name
+  }
+}
+
+# 2. Prometheus (The Metrics Database)
+resource "docker_container" "prometheus" {
+  name  = "prometheus"
+  image = "prom/prometheus:latest"
+  restart = "unless-stopped"
+
+  volumes {
+    host_path      = "${var.project_root}/monitoring/prometheus.yml"
+    container_path = "/etc/prometheus/prometheus.yml"
+  }
+
+  networks_advanced {
+    name = docker_network.aether_network.name
+  }
+}
+
+# 3. Grafana (The Dashboard)
+resource "docker_container" "grafana" {
+  name  = "grafana"
+  image = "grafana/grafana:latest"
+  restart = "unless-stopped"
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+
+  networks_advanced {
+    name = docker_network.aether_network.name
+  }
+}
